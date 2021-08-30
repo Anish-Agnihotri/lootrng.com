d48df010e0c5bf520aac20ef9f51565298e7c1bb
Anish Agnihotri <contact@anishagnihotri.com>
2021-08-30 12:26:43 -0400

    Pushing application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// Imports
+import "@styles/global.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Types
+import type { AppProps } from "next/app";
+
+// Export application
+export default function LootRNG({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-export default MyApp
